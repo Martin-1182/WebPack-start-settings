fbee093d2c75f910ac24c38198780037df3ea15e
Martin Halaj <m.halaj11@gmail.com>
2019-12-23 11:55:13 +0100

    ‰ commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,16 @@
-import { gsap } from "gsap";
+import { TweenMax, TweenLite } from "gsap";
+
+import AOS from "aos";
+AOS.init();
+
+// Animation
+document.addEventListener("DOMContentLoaded", function() {
+	TweenLite;
+	TweenMax.to("#git", 1, {
+    opacity: 1,
+    x: 0,
+    delay: 1,
+    duration: 2.5,
+    ease: "elastic.out(1, 0.3)"
+  });
+});
